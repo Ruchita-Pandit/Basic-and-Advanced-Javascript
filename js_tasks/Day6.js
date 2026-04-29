adfe3fbf1d94308139ee21dd104d1cb61c3cf69b
Ruchita Pandit (G10X <ruchita.pandit@g10x.com>
2026-04-29 14:45:17 +0530

    created readme

---
+++ js_tasks/Day6.js
@@ -0,0 +1,10 @@
+//TASK 1
+function area(len,width){
+    return 2*(len+width);
+}
+const rec1_area=area(2,3);
+console.log("Rectangle 1 Area : ",rec1_area);
+const rec2_area=area(5,10);
+console.log("Rectangle 2 Area : ",rec2_area);
+const rec3_area=area(1,9);
+console.log("Rectangle 3 Area : ",rec3_area);
