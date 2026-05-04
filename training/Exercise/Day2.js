00308235ad58dea1ebb2520844b499445bb04a1e
ruchita pandit <1ruchitapandit@gmail.com>
2026-05-04 17:56:52 +0530

    Add content delete file

---
+++ training/Exercise/Day2.js
@@ -0,0 +1,14 @@
+onsole.log(typeof "Hello");
+console.log(typeof 42);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof Symbol());
+
+console.log("\n")
+console.log(5 + "3");
+console.log("5" + 3);
+console.log("10" - 4);
+console.log("abc" - 1);
+console.log(true + 1);
+console.log(false + 1);
--- training/classwork/Day2.js
+++ training/classwork/Day2.js
@@ -14,16 +14,3 @@ console.log(greeting.slice(7));*/
 
 /*const greeting = " Hello, JavaScript! ";
 console.log(greeting.trim().toUpperCase().slice(0,5));*/
-
-
-//TASK 1
-const str="hey";
-const age=21;
-const bol=true;
-const assignNull=null;
-let Name;
-console.log("VALUE : ",str,"TYPE : ",typeof(str))
-console.log("VALUE : ",age,"TYPE : ",typeof(age))
-console.log("VALUE : ",bol,"TYPE : ",typeof(bol))
-console.log("VALUE : ",assignNull,"TYPE : ",typeof(assignNull))
-console.log("VALUE : ",Name,"TYPE : ",typeof(Name))
